01348fc9faa18a49062c37723556e0b8285c5cc0
Michael Shushkovsky <Michaels1410@nycstudents.net>
2023-12-01 11:28:10 -0500

    Commit #2 (broken)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi far han(solo)");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Hello, and welcome to the brain game party pack!!!");
+        System.out.println("You can pick between a trivia game, a math quiz, or placeholder!");
+        System.out.print("Select one now (trivia, math, or placeholder): ");
+        String selectedChoice = scan.nextLine();
+
+        if (selectedChoice.toLowerCase().equals("trivia")){
+            System.out.print("You selected the Trivia game! Select your category: history, computer science, or movies? ");
+            scan.nextLine();
+            String type = scan.nextLine();
+            Trivia trivia = new Trivia(type);
+        }
     }
 }
