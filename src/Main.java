35300c19dc4ae62b2a2bbc6db2b1fb883b3e9fe6
Michael Shushkovsky <Michaels1410@nycstudents.net>
2023-12-02 18:05:46 -0500

    History almost completed commit

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,47 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        boolean testComplete = false;
+
         System.out.println("Hello, and welcome to the brain game party pack!!!");
-        System.out.println("You can pick between a trivia game, a math quiz, or placeholder!");
-        System.out.print("Select one now (trivia, math, or placeholder): ");
+        System.out.println("You can pick between a history trivia, a math quiz, or hangman!");
+        System.out.print("Select one now (history, math, or hangman): ");
         String selectedChoice = scan.nextLine();
 
-        if (selectedChoice.toLowerCase().equals("trivia")){
-            System.out.print("You selected the Trivia game! Select your category: history, computer science, or movies? ");
-            scan.nextLine();
-            String type = scan.nextLine();
-            Trivia trivia = new Trivia(type);
+        if (selectedChoice.toLowerCase().equals("history")){
+            System.out.println("Welcome to the history trivia!");
+            System.out.println();
+            System.out.println("You will be able to pick questions from 5 difficulties, and completing a question of each difficulty gives you more points per difficulty increase.");
+            System.out.println("Remember, spelling WILL be important for this trivia.");
+            System.out.println("Good luck!");
+            Trivia history = new Trivia();
+            int questionNumber = 0;
+            int pointsEarned = 0;
+
+            while (questionNumber < 5){
+                questionNumber++;
+                int randomOneToFour = (int)(Math.random() * 5);
+                System.out.print("What difficulty would you like to select?(1 through 5) ");
+                int selectedDifficulty = scan.nextInt();
+                System.out.println("Question number " + questionNumber + ".");
+                System.out.println("Difficulty level: " + selectedDifficulty);
+                System.out.println("Your question is: " + history.getQuestion(selectedDifficulty - 1, randomOneToFour));
+                System.out.print("Answer here: ");
+                scan.reset();
+                String answer = scan.nextLine();
+                if (history.checkAnswer(selectedDifficulty - 1, randomOneToFour, answer)){
+                    System.out.println("Correct! You get " + (selectedDifficulty * 100 + " points."));
+                    pointsEarned += (selectedDifficulty * 100);
+                    System.out.println();
+                } else{
+                    System.out.println("Wrong. The correct answer was: \"" + history.getAnswer(selectedDifficulty - 1, randomOneToFour) + "\".");
+                    System.out.println();
+                }
+            }
+        } else if(selectedChoice.toLowerCase().equals("math")){
+            System.out.print("Welcome to the math quiz!");
+        } else if(selectedChoice.toLowerCase().equals("hangman")){
+            System.out.print("Welcome to hangman!");
         }
     }
 }
--- src/Trivia.java
+++ src/Trivia.java
@@ -1,16 +1,32 @@
 public class Trivia {
-    public Trivia(String type){
+    public Trivia(){
 
     }
 
     String[][] historyQuestions = {
-            {"Who was the dictator of Germany during World War II?", "What country originally colonized Canada?", "What American president issued the Emancipation Proclamation?", "Who led the British colonies in North America to a victory in the revolutionary war?", "Who was the first president of the United States of America?"},
+            {"Who was the dictator of Germany during World War II?", "What country originally colonized Canada?", "What American president issued the Emancipation Proclamation?", "Who was the first american president?", "Who was the scientist that created the equation: \"e = mc^2\"?"},
             {"Who did John Wilkes Booth assassinate?", "What was the name of the first civilization?", "Who invented the telephone?", "What was the first country to have gunpowder?", "Who is regarded as \"The Father of communism\"?"},
-            {"Who was the 34th president of the United States of America?", "Who was the leader of the Viet Cong during the Vietnam war?", "What is the name of the racial discrimination in South Africa?", "Who was the leader of the Soviet Union during World War II?", "What Spanish explorer conquered the Aztecs?"},
+            {"Who was the 24th president of the United States of America?", "Who was the leader of the Viet Minh during the Vietnam war?", "What is the name of the racial discrimination in South Africa?", "Who was the leader of the Soviet Union during World War II?", "Where was Christopher Columbus trying to travel to when he landed in America?"},
             {"In what year did the american Civil War start?", "Who invented the first computer?", "What year did the Boston Massacre occur in?", "What was the name of the second Japanese city that America bombed near the end of WWII?", "In what year did the USSR disband?"},
-            {"Who proposed that the constitution of the United States of America have a bill of rights?", "What is the form of government of the U.K?", "Who was the leader of the russian revolution?", "Who was the Chinese emperor that started construction of the Great Wall of China?", "Who is commonly referred to as the person who created the first printing press?"}};
+            {"Who proposed that the constitution of the United States of America have a bill of rights?", "What is the form of government of the U.K?", "Who was the leader of the russian revolution?", "Who was the Chinese emperor that started construction of the Great Wall of China?", "Who is commonly referred to as the person who created the first printing press?"}
+    };
 
-    public String getHistoryQuestions(int array, int question) {
+    String[][] historyAnswers = {
+            {"Adolf Hitler", "France", "Abraham Lincoln", "George Washington", "Albert Einstein"},
+            {"Abraham Lincoln", "Mesopotamia", "Alexander Graham Bell", "China", "Karl Marx"},
+            {"Grover Cleveland", "Ho Chi Minh", "Apartheid", "Joseph Stalin", "India"},
+            {"1861", "Charles Babbage", "1770", "Nagasaki", "1991"},
+            {"George Mason", "Constitutional Monarchy", "Vladimir Lenin", "Qin Shi Huang", "Johannes Gutenberg"}
+    };
+
+    public String getQuestion(int array, int question) {
         return historyQuestions[array][question];
+    }
+    public String getAnswer(int array, int question) {
+        return historyAnswers[array][question];
+    }
+
+    public boolean checkAnswer(int array, int question, String answer){
+        return getAnswer(array, question).toLowerCase().equals(answer.toLowerCase());
     }
 }
