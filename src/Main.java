cce4fb245456c1386e984317179699fa0d3febe8
narhaF <farhank9@nycstudents.net>
2023-12-03 15:06:55 -0500

    FINISHED MATH CLASS + RUNNER

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,118 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        boolean run = true;
+        boolean math = false;
+        boolean trivia = false;
+        boolean hangman = false;
 
         boolean testComplete = false;
 
-        System.out.println("Hello, and welcome to the brain game party pack!!!");
-        System.out.println("You can pick between a history trivia, a math quiz, or hangman!");
-        System.out.print("Select one now (history, math, or hangman): ");
-        String selectedChoice = scan.nextLine();
+        while (run) {
+            System.out.println("Hello, and welcome to the brain game party pack!!!");
+            System.out.println("You can pick between a history trivia, a math quiz, or hangman!");
+            System.out.print("Select one now (history, math, or hangman, or 'quit' to exit): ");
+            String selectedChoice = scan.nextLine();
+            if (selectedChoice.toLowerCase().equals("history")){
+                trivia = true;
 
-        if (selectedChoice.toLowerCase().equals("history")){
-            System.out.println("Welcome to the history trivia!");
-            System.out.println();
-            System.out.println("You will be able to pick questions from 5 difficulties, and completing a question of each difficulty gives you more points per difficulty increase.");
-            System.out.println("Remember, spelling WILL be important for this trivia.");
-            System.out.println("Good luck!");
-            Trivia history = new Trivia();
-            int questionNumber = 0;
-            int pointsEarned = 0;
+            } else if(selectedChoice.toLowerCase().equals("math")){
+                math = true;
+            } else if(selectedChoice.toLowerCase().equals("hangman")){
+                hangman = true;
+                System.out.print("Welcome to hangman!");
+            } else if (selectedChoice.toLowerCase().equals("quit")) {
+                run = false;
+            }
+            while (trivia) {
+                System.out.println("Welcome to the history trivia!");
+                System.out.println();
+                System.out.println("You will be able to pick questions from 5 difficulties, and completing a question of each difficulty gives you more points per difficulty increase.");
+                System.out.println("Remember, spelling WILL be important for this trivia.");
+                System.out.println("Good luck!");
+                Trivia history = new Trivia();
+                int questionNumber = 0;
+                int pointsEarned = 0;
 
-            while (questionNumber < 5){
-                questionNumber++;
-                int randomOneToFour = (int)(Math.random() * 5);
-                System.out.print("What difficulty would you like to select?(1 through 5) ");
-                int selectedDifficulty = scan.nextInt();
-                System.out.println("Question number " + questionNumber + ".");
-                System.out.println("Difficulty level: " + selectedDifficulty);
-                System.out.println("Your question is: " + history.getQuestion(selectedDifficulty - 1, randomOneToFour));
-                System.out.print("Answer here: ");
-                scan.reset();
-                String answer = scan.nextLine();
-                if (history.checkAnswer(selectedDifficulty - 1, randomOneToFour, answer)){
-                    System.out.println("Correct! You get " + (selectedDifficulty * 100 + " points."));
-                    pointsEarned += (selectedDifficulty * 100);
-                    System.out.println();
-                } else{
-                    System.out.println("Wrong. The correct answer was: \"" + history.getAnswer(selectedDifficulty - 1, randomOneToFour) + "\".");
-                    System.out.println();
+                while (questionNumber < 5){
+                    questionNumber++;
+                    int randomOneToFour = (int)(Math.random() * 5);
+                    System.out.print("What difficulty would you like to select?(1 through 5) ");
+                    int selectedDifficulty = Integer.parseInt(scan.nextLine());
+                    System.out.println("Question number " + questionNumber + ".");
+                    System.out.println("Difficulty level: " + selectedDifficulty);
+                    System.out.println("Your question is: " + history.getQuestion(selectedDifficulty - 1, randomOneToFour));
+                    System.out.print("Answer here: ");
+                    scan.reset();
+                    String answer = scan.nextLine();
+                    if (history.checkAnswer(selectedDifficulty - 1, randomOneToFour, answer)){
+                        System.out.println("Correct! You get " + (selectedDifficulty * 100 + " points."));
+                        pointsEarned += (selectedDifficulty * 100);
+                        System.out.println();
+                    } else{
+                        System.out.println("Wrong. The correct answer was: \"" + history.getAnswer(selectedDifficulty - 1, randomOneToFour) + "\".");
+                        System.out.println();
+                    }
+                }
+                System.out.println("Game over.");
+                System.out.println("Score: " + pointsEarned);
+                System.out.println("-------------------------------\n");
+                trivia = false;
+            }
+            while (math) {
+                int a,b,c;
+                a = (int)(Math.random()*201-100);
+                b = (int)(Math.random()*201-100);
+                c = (int)(Math.random()*201-100);
+                MathGame m = new MathGame(a, b, c);
+                System.out.println();
+                System.out.println("---------------------------");
+                System.out.println("1. Linear Equations");
+                System.out.println("2. System of Equations");
+                System.out.println("3. Area of Triangles");
+                System.out.println("4. Area of Trapezoids");
+                System.out.println("5. Volume of Rectangular Prisms");
+                System.out.println("6. Volume of Square-Based Pyramids");
+                System.out.println("---------------------------");
+                System.out.println("REMEMBER! All answers are rounded to the nearest HUNDREDTH");
+                System.out.println("Select a problem type. (ex: 1 or 'one')");
+                String mathChoice = scan.next();
+                if (mathChoice.equals("1") || mathChoice.equals("one")) {
+                    m.linearEquations();
+                    double answer = scan.nextDouble();
+                    m.checkLinear(answer);
+                } else if (mathChoice.equals("2") || mathChoice.equals("two")) {
+                    m.systemOfEquations();
+                    System.out.println("Enter x value: ");
+                    double answerX = scan.nextDouble();
+                    System.out.println("Enter y value: ");
+                    double answerY = scan.nextDouble();
+                    m.checkSystem(answerX, answerY);
+                } else if (mathChoice.equals("3") || mathChoice.equals("three")) {
+                    m.triangleArea();
+                    double answer = scan.nextDouble();
+                    m.checkTri(answer);
+                } else if (mathChoice.equals("4") || mathChoice.equals("four")) {
+                    m.trapezoidArea();
+                    double answer = scan.nextDouble();
+                    m.checkTrap(answer);
+                } else if (mathChoice.equals("5") || mathChoice.equals("five")) {
+                    m.prismVolume();
+                    double answer = scan.nextDouble();
+                    m.checkPrism(answer);
+                } else if (mathChoice.equals("6") || mathChoice.equals("six")) {
+                    m.pyramidVolume();
+                    double answer = scan.nextDouble();
+                    m.checkPyramid(answer);
+                }
+                System.out.println("Play again? y/n");
+                String replayMath = scan.next();
+                if (replayMath.equals("n")) {
+                    math = false;
                 }
             }
-        } else if(selectedChoice.toLowerCase().equals("math")){
-            System.out.print("Welcome to the math quiz!");
-        } else if(selectedChoice.toLowerCase().equals("hangman")){
-            System.out.print("Welcome to hangman!");
         }
+        System.out.println("Thank you for playing!");
+
     }
 }
--- src/MathGame.java
+++ src/MathGame.java
@@ -1,19 +1,52 @@
 public class MathGame {
-
     private int a;
     private int b;
     private int c;
-
+    private int d;
+    private int e;
+    private int f;
     public MathGame(int a, int b, int c){
         this.a = a;
         this.b = b;
         this.c = c;
     }
-//    String[][] quadratics =  {
-//            {"3x^2 + x = 0", "-3x^2 + x + 4 = 0", "x^2 + 5x - 14 = 0"},
-//            {"0",}
-//    }
+    public void systemOfEquations() {
+        this.d = (int)(Math.random()*201-100);
+        this.e = (int)(Math.random()*201-100);
+        this.f = (int)(Math.random()*201-100);
 
+        System.out.println("Solve for x and y.\n");
+        String equation1 = a + "x ";
+        if (b >0) {
+            equation1 += "+ " + b;
+        } else if (b < 0){
+            equation1 += b;
+        }
+        equation1 += "y = " + c;
+
+        String equation2 = d + "y ";
+        if (e >0) {
+            equation2 += "+ " + e;
+        } else if (e < 0) {
+            equation2 += e;
+        }
+        equation2 += "x = " + f;
+
+        System.out.println(equation1 + "\n" + equation2);
+    }
+    public double systemAnswerX() {
+        return Math.round(((double)(c*d-b*f)/(a*d-b*e))*100)/100.0;
+    }
+    public double systemAnswerY() {
+        return Math.round(((double)(a*f-e*c)/(a*d-b*e))*100)/100.0;
+    }
+    public void checkSystem(double x, double y) {
+        if (x == systemAnswerX() && y == systemAnswerY()) {
+            System.out.println("Correct! x = " + systemAnswerX() + ", y = " + systemAnswerY());
+        } else {
+            System.out.println("Incorrect. x = " + systemAnswerX() + ", y = " + systemAnswerY());
+        }
+    }
     public void linearEquations() {
         String equation = "Solve for x: ";
         equation += a + "x ";
@@ -25,11 +58,9 @@ public class MathGame {
         equation += " = " + c;
         System.out.println(equation);
     }
-
     public double linearAnswer() {
-        return (double)(c - b)/a;
+        return Math.round(((double)(c - b)/a)*100)/100.0;
     }
-
     public void checkLinear(double answer) {
         if (answer == linearAnswer()) {
             System.out.println("Correct! x = " + linearAnswer());
@@ -37,16 +68,12 @@ public class MathGame {
             System.out.println("Incorrect. x = " + linearAnswer());
         }
     }
-
-
     public void triangleArea() {
-        System.out.println("What is the area of a triangle with length" + a + " and height " + b + "?");
+        System.out.println("What is the area of a triangle with length " + Math.abs(a) + " and height " + Math.abs(b) + "?");
     }
-
     public double areaTriangle() {
-        return (a*b)/2.0;
+        return Math.round((Math.abs(a*b)/2.0)*100)/100.0;
     }
-
     public void checkTri(double answer) {
         if (answer == areaTriangle()) {
             System.out.println("Correct! The answer is " + areaTriangle());
@@ -54,19 +81,46 @@ public class MathGame {
             System.out.println("Incorrect. The answer is " + areaTriangle());
         }
     }
-
     public void trapezoidArea() {
-        System.out.println("What is the area of a trapezoid with base lengths of " + a + " and " + b + " and height " + c + "?");
+        System.out.println("What is the area of a trapezoid with base lengths of " + Math.abs(a) + " and " + Math.abs(b) + " and height " + Math.abs(c) + "?");
     }
     public double areaTrapezoid() {
-        return ((a+b)/2.0)*c;
+        return Math.round((((((Math.abs(a)+Math.abs(b))*c/2.0))*100)))/100.0;
     }
-
     public void checkTrap(double answer) {
         if (answer == areaTrapezoid()) {
             System.out.println("Correct! The answer is " + areaTrapezoid());
         } else {
             System.out.println("Incorrect. The answer is " + areaTrapezoid());
+        }
+    }
+    public void prismVolume() {
+        System.out.println("What is the volume of a rectangular prism with length " + Math.abs(a) + ", width " + Math.abs(b) + ", and height " + Math.abs(c));
+    }
+    public double volumePrism() {
+        return Math.abs(a*b*c);
+    }
+    public void checkPrism(double answer) {
+        if (answer == volumePrism()) {
+            System.out.println("Correct! The answer is " + volumePrism());
+        } else {
+            System.out.println("Incorrect. The answer is " + volumePrism());
+        }
+    }
+
+    public void pyramidVolume() {
+        System.out.println("Find the volume of a square pyramid with base length " + Math.abs(a) + " and height " + Math.abs(b));
+    }
+
+    public double volumePyramid() {
+        return Math.round((Math.abs(((Math.pow(a, 2)))*b)/3)*100)/100.0;
+    }
+
+    public void checkPyramid(double answer) {
+        if (answer == volumePyramid()) {
+            System.out.println("Correct! The answer is " + volumePyramid());
+        } else {
+            System.out.println("Incorrect. The answer is " + volumePyramid());
         }
     }
 }
